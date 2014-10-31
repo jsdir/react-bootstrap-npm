17752955c6e07fc57e3930970763233402a2ef66
Jason Sommer <jsdirv@gmail.com>
2014-10-31 04:38:48 -0500

    Do not require the overlay prop in OverlayMixin

---
+++ ButtonToolbar.js
@@ -0,0 +1,30 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var classSet = require('./utils/classSet');
+var BootstrapMixin = require('./BootstrapMixin');
+var Button = require('./Button');
+
+var ButtonToolbar = React.createClass({displayName: 'ButtonToolbar',
+  mixins: [BootstrapMixin],
+
+  getDefaultProps: function () {
+    return {
+      bsClass: 'button-toolbar'
+    };
+  },
+
+  render: function () {
+    var classes = this.getBsClassSet();
+
+    return this.transferPropsTo(
+      React.DOM.div(
+        {role:"toolbar",
+        className:classSet(classes)}, 
+        this.props.children
+      )
+    );
+  }
+});
+
+module.exports = ButtonToolbar;
